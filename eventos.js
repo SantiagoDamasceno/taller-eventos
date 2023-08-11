cc20af6d5d3f8bb02a45eedce1d81da55dce3f78
snthze <santiagodamasc123@gmail.com>
2023-08-11 13:21:47 -0300

    Cambios definitivos

--- eventos.js
+++ eventos.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded",  function() {
     let btn = document.getElementById('btn');
     console.log(divs);
     console.log(btn);
-    btn.addEventListener('click', (event)=>{
-        event.stopPropagation();
-        alert('Hola! Soy el btn')
-    })
+    
     
     divs.addEventListener("click", () => {
         alert('Hola! Soy el div');
     });
+    btn.addEventListener('click', ()=>{
+        event.stopPropagation();
+        alert('Hola! Soy el btn')
+    })
 
 })
